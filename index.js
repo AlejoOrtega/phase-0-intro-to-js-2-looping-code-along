38dbbaa085f03f8cd4969fa0df32447dac44b069
Alejandro <ajrevollo@uninorte.edu.co>
2022-04-05 10:21:59 -0400

    commit tree c68e9e5e706e611a453bb52c2826f14e5ab5d3e2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, topic){
+    let holder =[]
+    for(let i=0; i < array.length; i++){
+        holder.push(`Thank you, ${array[i]}, for the wonderful ${topic} gift!`)
+    };
+    console.log(holder)
+    return holder;
+    
+}
+
+function countDown(number){
+    for (let index = number; 0 <= index; index--){
+        console.log(index);
+    }
+}
